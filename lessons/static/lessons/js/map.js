2441cbbf6c0514a59ccddd68aaab0973cccd2aa5
regina404 <regina11012002@gmail.com>
2021-12-26 14:57:01 +0200

    route between school and user

--- lessons/static/lessons/js/map.js
+++ lessons/static/lessons/js/map.js
@@ -6,6 +6,7 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 L.marker([50.450100,30.456977]).addTo(map).bindPopup("<strong>Національний технічний університет України «Київський політехнічний інститут імені Ігоря Сікорського»</strong><br />Адрес:проспект Перемоги, 37, Київ, 03056").openPopup();
 
+
 let distanceFromLocationsInKm = (lat1,lon1,lat2,lon2) => {
     const R = 6371; // Радиус земли
     const dLat = (lat2-lat1).toRad();
@@ -34,7 +35,30 @@ window.navigator.geolocation.getCurrentPosition(function(pos) {
 
     L.marker([pos.coords.latitude,pos.coords.longitude]).addTo(map).bindPopup("<strong>Ваше местоположение</strong><br />").openPopup();
     const polyline = L.polyline(locations, {color:'red'}).addTo(map)
+    var routeControl = L.Routing.control(
+      {
+       language: 'ru',
+       formatter:  new L.Routing.Formatter({
+        language: 'ru'
+       }),
+
+       waypoints: [
+        L.latLng(pos.coords.latitude,pos.coords.longitude),
+        L.latLng(50.450100, 30.456977),
+       ],
+       lineOptions:{
+        styles: [{color: 'blue', opacity: 1, weight: 5}],
+        addWaypoints:false
+       },
+       routeWhileDragging: true
+      }).addTo(map);
+      routeControl.on('routesfound', function(e) {
+        const routes = e.routes;
+        const summary = routes[0].summary;
+        document.getElementById("summarydist").innerHTML = "Расстояние между нами, учитывая лучший маршрут:" +" " + summary.totalDistance / 1000 +" " + "км";
+        document.getElementById("summarytime").innerHTML = "Примерное время в дороге:" +" " + Math.round(summary.totalTime % 3600 / 60) +" " + "минут";
+
+     });
     const ourDistanse = distanceFromLocationsInKm(50.450100, 30.456977,pos.coords.latitude,pos.coords.longitude);
-    const box = document.getElementById("distance");
-    box.innerHTML = "Расстояние между нами:" +" " + ourDistanse +" " + "км";
+    document.getElementById("distance").innerHTML = "Расстояние между нами:" +" " + ourDistanse +" " + "км";
 });
