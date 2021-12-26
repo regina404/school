7cc9d1a04687f95653628d1c3507b1eac94e4880
regina404 <regina11012002@gmail.com>
2021-12-26 13:31:14 +0200

    distance between school and user

--- lessons/static/lessons/js/map.js
+++ lessons/static/lessons/js/map.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([50.450100,30.456977], 15);
+const map = L.map('map').setView([50.450100,30.456977], 12);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -8,27 +8,33 @@ L.marker([50.450100,30.456977]).addTo(map).bindPopup("<strong>Національ
 
 let distanceFromLocationsInKm = (lat1,lon1,lat2,lon2) => {
     const R = 6371; // Радиус земли
-    const dLat = degToRad(lat2-lat1);
-    const dLon = degToRad(lon2-lon1);
+    const dLat = (lat2-lat1).toRad();
+    const dLon = (lon2-lon1).toRad();
     const a =
       Math.sin(dLat/2) * Math.sin(dLat/2) +
-      Math.cos(degToRad(lat1)) * Math.cos(degToRad(lat2)) *
+      Math.cos((lat1).toRad()) * Math.cos((lat2).toRad()) *
       Math.sin(dLon/2) * Math.sin(dLon/2)
       ;
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     const d = R * c; // Просчитываем дистанцим в киллометрах
     return d;
-  }
+}
 
-  let degToRad = (deg) => {
-    return deg * (Math.PI/180)
-  }
+if (typeof(Number.prototype.toRad) === "undefined") {
+    Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+    }
+}
 
-let locations = [
-    [50.450100, 30.456977],
-    [50.458, 30.457]
-]
-const distance = distanceFromLocationsInKm(locations[0][0],locations[0][1],locations[1][0],locations[1][1]);
+window.navigator.geolocation.getCurrentPosition(function(pos) {
+    const locations = [
+      [50.450100, 30.456977],
+      [pos.coords.latitude,pos.coords.longitude]
+    ]
 
-let polyline = L.polyline(locations, {color:'red'}).addTo(map)
-  console.log(distance);
+    L.marker([pos.coords.latitude,pos.coords.longitude]).addTo(map).bindPopup("<strong>Ваше местоположение</strong><br />").openPopup();
+    const polyline = L.polyline(locations, {color:'red'}).addTo(map)
+    const ourDistanse = distanceFromLocationsInKm(50.450100, 30.456977,pos.coords.latitude,pos.coords.longitude);
+    const box = document.getElementById("distance");
+    box.innerHTML = "Расстояние между нами:" +" " + ourDistanse +" " + "км";
+});
